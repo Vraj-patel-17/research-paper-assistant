92d69fc4a7703fbd79f9f4b4e802283fbe776ae8
Vraj Patel <vraj1308patel@gmail.com>
2026-06-27 13:35:56 +0530

    Setup PostgreSQL database connection with SQLAlchemy

---
+++ backend/app/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine,text
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+load_dotenv()
+DATABASE_URL=os.getenv("DATABASE_URL")
+engine=create_engine(DATABASE_URL)
+SessionLocal=sessionmaker(autoflush=False,autocommit=False,bind=engine)
+try:
+    with engine.connect() as conn:
+        conn.execute(text("Select 1"))
+        print("Databse connected successfullty!")
+
+except Exception as e:
+    print("Database connection failed!")
+    print(e)
