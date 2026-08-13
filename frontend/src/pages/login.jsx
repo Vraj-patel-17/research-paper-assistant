5919dcb5f134c8127cadc7a2b9cabffdb2014624
Vraj Patel <vraj1308patel@gmail.com>
2026-08-13 10:08:27 +0530

    fix backend docker networking and cors

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,5 +1,75 @@
+import { useState } from "react";
+import { setToken } from "../auth/auth";
 function Login() {
-  return <h1>Login</h1>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new URLSearchParams();
+
+    formData.append("username", email);
+    formData.append("password", password);
+
+    const response = await fetch(
+      `${import.meta.env.VITE_API_URL}/login`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: formData,
+      }
+    );
+
+    const data = await response.json();
+
+    if (!response.ok) {
+  console.log(data);
+  return;
+}
+
+const token = data.access_token;
+setToken(token);
+
+const bookmarksResponse = await fetch(
+  `${import.meta.env.VITE_API_URL}/bookmarks`,
+  {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+);
+
+const bookmarks = await bookmarksResponse.json();
+
+console.log("Bookmarks:", bookmarks);
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
